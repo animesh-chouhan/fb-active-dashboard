1e9e4989d3c9304ca966dbd539e507bb477384d1
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-05-16 13:39:03 +0530

    added chromium dependencies

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,19 @@ WORKDIR /usr/src/app
 # where available (npm@5+)
 COPY package*.json ./
 
-RUN npm install
+# Install libs and dependencies
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf \
+    --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
 # If you are building your code for production
 # RUN npm ci --only=production
+RUN npm install
 
 # Bundle app source
 COPY . .
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-"use strict";
+// "use strict";
 
-const express = require("express");
+// const express = require("express");
 
-// Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
+// // Constants
+// const PORT = 8080;
+// const HOST = "0.0.0.0";
 
-// App
-const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+// // App
+// const app = express();
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+
+    // Get the "viewport" of the page, as reported by the page.
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        };
+    });
+
+    console.log("Dimensions:", dimensions);
+
+    await browser.close();
+})();
