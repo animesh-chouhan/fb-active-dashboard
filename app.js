2d51f663d5c0b9c42bd85f99c347ea1b9ae8d766
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-05-17 07:59:31 +0530

    added dockerignore

--- app.js
+++ app.js
@@ -4,32 +4,21 @@ const express = require("express");
 const puppeteer = require("puppeteer-core");
 
 // Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
+const port = 8080;
+const host = "0.0.0.0";
 const app = express();
 
-(async () => {
-    const browser = await puppeteer.launch({ executablePath: "google-chrome-stable" });
-    const page = await browser.newPage();
-    await page.goto("https://example.com");
-
-    // Get the "viewport" of the page, as reported by the page.
-    const dimensions = await page.evaluate(() => {
-        return {
-            width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight,
-            deviceScaleFactor: window.devicePixelRatio,
-        };
+const sleep = async (ms) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, ms);
     });
-
-    console.log("Dimensions:", dimensions);
-
-    await browser.close();
-})();
+};
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
